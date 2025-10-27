b257aa31dd19e132cca557122179025829b2b257
Noobie33 <setu7090@gmail.com>
2025-10-27 14:26:38 +0600

    c++

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace test
+{class neme
+    { static void Main ()
+        {
+            System.Console.WriteLine("akibur rahman");
+            System.Console.WriteLine("ssc : national ideal school");
+            System.Console.WriteLine("hsc : cambrian college");
+             System.Console.WriteLine("intreasting fact: i don't have gf");
+    }
+    }
+}
